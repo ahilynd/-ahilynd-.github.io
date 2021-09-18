eafa696e359044d4af7923e8a78da6e08976a974
ahilynd <AHILYND@UDEL.EDU>
2021-09-18 14:09:38 -0400

    setting up nav bar contents

--- navigation-bar/src/components /Navbar/index.js
+++ navigation-bar/src/components /Navbar/index.js
@@ -7,3 +7,37 @@ import{
     NavBtn,
     NavBtnLink,
 } from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+          <Nav>
+              <Bars />
+
+              <NavMenu>
+                  <NavLink to='/about' activeStyle>
+                      About 
+                  </NavLink>
+                  <NavLink to='/change log' activeStyle>
+                      Change Log 
+                  </NavLink>
+                  <NavLink to='/contact' activeStyle>
+                      Contact
+                  </NavLink>
+                  <NavLink to='/movies' activeStyle>
+                      Movies
+                  </NavLink>
+                  <NavLink to='/my repository' activeStyle>
+                      My Repository
+                  </NavLink>
+                  <NavLink to='/resources' activeStyle>
+                      Resources
+                  </NavLink>
+                  <NavLink to='/todo list' activeStyle>
+                      ToDo Lists
+                  </NavLink>
+              </NavMenu>
+          </Nav>
+        </>
+    );
+};
