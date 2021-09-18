cc0909781651f909826c1c449a760b6244068418
ahilynd <AHILYND@UDEL.EDU>
2021-09-18 14:20:52 -0400

    navbar style

---
+++ navigation-bar/src/components /Navbar/NavBarElements.js
@@ -0,0 +1,12 @@
+import { FaBars } from 'react-icons/fa';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  background: #63D471;
+  height: 85px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
+`;
