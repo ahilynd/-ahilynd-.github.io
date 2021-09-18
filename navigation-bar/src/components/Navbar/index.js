4b0a23d2f477ea3f4701fc622ccbe7a01ac7d45b
ahilynd <AHILYND@UDEL.EDU>
2021-09-18 19:33:11 -0400

    navlinks

--- navigation-bar/src/components/Navbar/index.js
+++ navigation-bar/src/components/Navbar/index.js
@@ -1,10 +1,42 @@
 import React from 'react'
+//import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <>
+        <Nav>
+            <NavLink to="/">
+                <h1>Logo</h1>
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to="/about" activeStyle>
+                    About
+                </NavLink>
+                <NavLink to='/change log' activeStyle>
+                     Change Log
+                 </NavLink>
+                 <NavLink to='/contact' activeStyle>
+                     Contact
+                 </NavLink>
+                 <NavLink to='/movies' activeStyle>
+                     Movies
+                 </NavLink>
+                 <NavLink to='/my repository' activeStyle>
+                     My Repository
+                 </NavLink>
+                 <NavLink to='/resources' activeStyle>
+                     Resources
+                 </NavLink>
+                 <NavLink to='/todo list' activeStyle>
+                     ToDo Lists
+                 </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+            </NavBtn>
+        </Nav>
+        </>
     );
 };
 
